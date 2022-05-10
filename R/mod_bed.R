7b20159bea4942a802fe3e7d663f34a77a071a60
CorradoLanera <corrado.lanera@gmail.com>
2022-05-10 16:40:22 +0200

    test connection

--- R/mod_bed.R
+++ R/mod_bed.R
@@ -12,14 +12,27 @@ mod_bed_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      list_to_p(c("Foo", "Bar")),
+      list_to_p(c(
+        "In this interface, you can start and stop record bed signals"
+      )),
+
       box(
-        title = "Inputs", status = "warning", solidHeader = TRUE,
-        textInput(ns("foo"), "Foo: ")
+        title = "Parameters", status = "warning", solidHeader = TRUE,
+        textInput(ns("pid"), "Person's ID: "),
+        selectInput(
+          ns("bedPort"),
+          label = "Bed connection port: ",
+          choices = serial::listPorts(),
+          selected = serial::listPorts()[[1L]]
+        ),
+        actionButton(ns("bedTestConnect"), "Test connection"),
+
+        actionButton(ns("bedStart"), "Start"),
+        actionButton(ns("bedStop"), "Stop"),
       ),
       box(
         title = "Output", status = "info", solidHeader = TRUE,
-        textOutput(ns("out_txt"))
+        DT::DTOutput(ns("con")),
       )
     )
   )
@@ -29,8 +42,33 @@ mod_bed_ui <- function(id){
 #'
 #' @noRd
 mod_bed_server <- function(id){
+
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    port <- reactive(
+      req(input[["bedPort"]])
+    )
+
+    test_out <- eventReactive(input[["bedTestConnect"]], {
+
+      con <- reactive(
+        iobed.bed::bed_connection(port())
+      )
+
+      Sys.sleep(1)
+
+      iobed.bed::pull_bed_stream(con()) |>
+        iobed.bed::tidy_iobed_stream()
+
+    })
+
+    output$con <- renderDataTable(tibble::as_tibble(test_out()))
+    # output$output <- renderDataTable({test_out()})
+
+    output$is_connected <- renderText({
+      glue::glue()
+    })
 
     output$out_txt <- renderText({
       input[["foo"]]
